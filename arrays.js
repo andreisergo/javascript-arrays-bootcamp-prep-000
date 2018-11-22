98d0f386d562228c8af41153766111eac93afd97
Andrei Sergo <a.sergoucht@gmail.com>
2018-11-22 00:19:46 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -16,8 +16,8 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray(array, element) {
-  array = [...array, element]
-  return array
+  return [...array, element]
+   
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
